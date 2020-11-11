8f40717ed6298323003c7ba2f6cfd9e5e072e9cb
opilane <opilane@HP-CZC6297B8P>
2020-11-11 09:13:03 +0200

    spech

--- Xamarin_elemendid_DM/Xamarin_elemendid_DM/Entry_P.xaml.cs
+++ Xamarin_elemendid_DM/Xamarin_elemendid_DM/Entry_P.xaml.cs
@@ -9,24 +9,37 @@ using Xamarin.Forms.Xaml;
 
 namespace Xamarin_elemendid_DM
 {
+    float Volume_;
+    float Pitch_;
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Entry_P : ContentPage
     {
         public Entry_P()
         {
-
+            InitializeComponent();
+            Volume_ = (float)vol.Value;
+            Pitch_ = (float)pit.Value;
         }
 
         public async void Button_Clicked(object sender, EventArgs e)
-
+        SpeechOptions settings = new SpeechOptions()
         {
-            InitializeComponent();
+
+            
             Volume = Volume_,
             Pitch = Pitch_,
         };
         if (text.Text !=null) { await Xamarin.Essentials.TextToSpeach.SpeakAsync(text.Text, settings); }
+        else if (text.Text !=null) { await Xamarin.Essentials.TextToSpeach.SpeakAsync(text.Placeholder, settings); }
+
         private void Slider_ValueChanged(object sender, ValueChangedEventArgs)
-            )
+        {
+            Volume_ = (float).vol.Value;
+        }
+        private void Slider_ValueChanged1(object sender, ValueChangedEventArgs)
+        {
+            Pitch_ = (float).pit.Value;
+        }
 
     }
 }
